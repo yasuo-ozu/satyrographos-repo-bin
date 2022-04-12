63f71ab95a1e7a31fb456cfc0696f12bb3a2f54b
yozu <yasuo@ozu.email>
2022-04-12 15:08:31 +0900

    init

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEST_REPO_DIR="$1"
+DEST_STORE_DIR="$2"
+DEST_ARCHIVES_DIR="$DEST_STORE_DIR/archives"
+LOCAL_OPAM_BIN_STORE="$HOME/.opam/plugins/opam-bin/store"
+
+rm -rf "$LOCAL_OPAM_BIN_STORE/repo" "$LOCAL_OPAM_BIN_STORE/archives"
+mkdir -p "$DEST_REPO_DIR" "$DEST_ARCHIVES_DIR"
+ln -snf "$DEST_REPO_DIR" "$LOCAL_OPAM_BIN_STORE/repo"
+ln -snf "$DEST_ARCHIVES_DIR" "$LOCAL_OPAM_BIN_STORE/archives"
+
+PACKAGES="satysfi satyrographos"
+
+eval $(opam env) && opam-bin config --base-url "https://raw.githubusercontent.com/yasuo-ozu/satyrographos-repo-bin/main"
+
+opam list --columns=package -V $PACKAGES | sed -e '/^#/d' | \
+while read PKGNAME; do
+	echo "Installing $PKGNAME"
+	opam install "$PKGNAME" -v -y && opam remove -a -y "$PKGNAME" || true
+done
