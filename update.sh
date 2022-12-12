70bcbcaaa5de87617c1fbc799756992f80f338db
yozu <yasuo@ozu.email>
2022-12-13 03:27:09 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -23,7 +23,7 @@ if where gsed &>/dev/null; then
 fi
 
 mkdir -p "$DEST_ARCHIVES_DIR" "$DEST_PACKAGES_DIR"
-
+ 
 if [[ "$TARGET_OS" = "Windows" ]]; then
 	if [[ "$TARGET_ARCH" = "X86" ]]; then
 		TARGET_OS="win32"
